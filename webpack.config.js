ec706829faf17feacbc70c9e131a64aa2e66808d
av3000 <alanas.vaiciulis@gmail.com>
2024-04-15 19:57:11 +0300

    feat: add angular-builders/custom-webpack setup for future customizations

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  resolve: {
+    modules: ["src", "node_modules"],
+  },
+  rules: [],
+  plugins: [],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+    hot: true,
+  },
+};
